93c8590266e46a77b8064edd13d48cba66b6dc3d
NutchaDan <nutcha.siriwut@gmail.com>
2021-11-27 15:01:12 +0700

    Hola

--- database/views.py
+++ database/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Book
 
+
 def index(request):
-    latest_book_list = Book.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.book_text for q in latest_book_list])
-    return HttpResponse(output)
+    latest_question_list = Book.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('database/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, book_id):
-    return HttpResponse("You're looking at question %s." % book_id)
-
+    return HttpResponse("You're looking at book %s." % book_id)
