09c8634df0250bbf0a41b6438f0fbd8ea2015787
NutchaDan <nutcha.siriwut@gmail.com>
2021-11-27 14:36:56 +0700

    first commit

--- database/views.py
+++ database/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
-
+from .models import Book
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_book_list = Book.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.book_text for q in latest_book_list])
+    return HttpResponse(output)
+
+def detail(request, book_id):
+    return HttpResponse("You're looking at question %s." % book_id)
+
