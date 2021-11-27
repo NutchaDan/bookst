50246a3e4ace38309bc42becbda154cec32f5523
NutchaDan <nutcha.siriwut@gmail.com>
2021-11-27 17:14:35 +0700

    commit

--- database/admin.py
+++ database/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Choice, Book
 
-admin.site.register(Book)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Book name',        {'fields': ['book_text']}),
+        ('Date information', {'fields': ['loan_date']}),
+        ('URL Image',        {'fields': ['url_image']}),
+        ('User',             {'fields': ['name_user']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Choice)
